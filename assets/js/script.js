4de406412801423b8541b651714f922033588e99
ldholl <lacy.d.holleman@gmail.com>
2022-09-07 06:30:36 -0400

    got save function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,20 +30,20 @@ var timeBlocks = document.querySelectorAll(".time-block");
 
 
 //function to check if past, present, or future
-var checkBlock = function (){
+var checkBlock = function () {
     //loops through all of the time blocks
-    for (i = 0; i < timeBlocks.length; i++){
+    for (i = 0; i < timeBlocks.length; i++) {
 
         var timeSlot = timeBlocks[i].id;
         timeSlot = parseInt(timeSlot);
         var thisTime = timeBlocks[i];
 
-        if(timeSlot > currentTime){
+        if (timeSlot > currentTime) {
             thisTime.classList.remove("present")
             thisTime.classList.remove("past")
             thisTime.classList.add("future")
         }
-        else if (timeSlot === currentTime){
+        else if (timeSlot === currentTime) {
             thisTime.classList.remove("future")
             thisTime.classList.add("present")
         }
@@ -51,12 +51,13 @@ var checkBlock = function (){
             thisTime.classList.remove("present")
             thisTime.classList.add("past")
         }
-}};
+    }
+};
 
 checkBlock();
 
 //Interval to recheck times every 30 minutes
-setInterval(function(){
+setInterval(function () {
     checkBlock();
 }, (1000 * 60) * 30);
 
@@ -67,83 +68,81 @@ var scheduleInputs = document.querySelectorAll("textarea");
 var saveBtns = document.querySelectorAll(".saveBtn");
 
 var schedulePairs = [{
-    
+
     button: "btn9",
-    textarea: $("#text9"),
-    inputText: $("#text9").val()
+    textarea: $("#text9")
 },
 {
     button: "btn10",
-    textarea: $("#text10"),
-    inputText: $("#text10").val()
+    textarea: $("#text10")
 },
 {
     button: "btn11",
-    textarea: $("#text11"),
-    inputText: $("#text11").val()
+    textarea: $("#text11")
 }];
 
-var schedule
+
 
 //FUNCTION to load slots
-var loadSchedule = function(){
-    schedule = JSON.parse(localStorage.getItem("schedule"));
+var loadSchedule = function () {
+    debugger
+    var schedule = JSON.parse(localStorage.getItem("schedule"));
 
-    if (!schedule){
+    if (!schedule) {
         schedule = [
             nineAm = "",
             tenAm = "",
-            elevenAm = ""
+            elevenAm = "",
         ];
-    }   
+    }
 
-  for (i = 0; i < schedulePairs.length; i++){
-    var insert = schedule[i].value;
-    var goalPlace = schedulePairs[i].textarea
-    
-    goalPlace.val(insert);
-  }};
+    for (i = 0; i < schedulePairs.length; i++) {
+        var insert = schedule[i].value;
+        var goalSlot = schedulePairs[i].textarea;
+
+        goalSlot.val(insert);
+        console.log(schedule);
+        localStorage.setItem("schedule", JSON.stringify(schedule));
+
+    }
+};
+
+loadSchedule();
 
 
+//When save button is clicked, its connected text is saved to local storage
+$(".saveBtn").click(function () {
+    console.log("click")
 
-$(".saveBtn").click (function(){
-console.log("click")
-    debugger
-    var clickedId = $(this).id;
-    var newText = $(this).siblings('.description')
+    var clickedId = this.id
+    var newText = $(this).siblings(".description").val();
     console.log("newText", newText);
-    console.log(newText.val(), "newText val()")
-   
-    
-    //$(this).closest("textarea").val();
 
-    for (i = 0; i < schedulePairs.length; i++){
+
+    for (i = 0; i < schedulePairs.length; i++) {
 
         var slots = schedulePairs[i].button;
-        
-        if (clickedId !== slots ){
-           console.log("not a match")
+
+        if (clickedId !== slots) {
+            console.log("not a match")
         }
-        else{
-            var newText = schedulePairs[i].inputText
-            console.log(newText.value);
-            
+        else {
+            console.log("match");
+
+            schedule = JSON.parse(localStorage.getItem("schedule"))
+            console.log(schedule);
+
+
+            schedule[i] = newText;
+            localStorage.setItem("schedule", JSON.stringify(schedule));
+            console.log(schedule);
+
+            break;
+
         }
     }
 }
 )
 
-loadSchedule();
 
 
-//WHEN I click the save button for that time block -THEN the text for that event is saved in local storage
-/*
-$(saveBtns).on("click", saveSchedule);
-
-var saveSchedule = function(event){
-    event.preventDefault();
-    
-
-}*/
-
-//WHEN I refresh the page -THEN the saved events persist
